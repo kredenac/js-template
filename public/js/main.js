fe7a5a450d64a43b105c70be10562d2978b78e13
Nikola Dimitrijevic <zacementirano@gmail.com>
2020-08-11 18:43:15 +0200

    added img upload to change bg

--- public/js/main.js
+++ public/js/main.js
@@ -36,3 +36,43 @@ const addMessage = (msg) => {
     message.innerHTML = typeof (msg) === "object" ? JSON.stringify(msg) : msg;
     chat.appendChild(message);
 };
+
+document.getElementById("imginput").addEventListener('change', readURL, true);
+
+function readURL() {
+    const files = document.getElementById("imginput").files;
+    const file = files[files.length - 1];
+    const reader = new FileReader();
+    reader.onloadend = function () {
+        document.getElementsByClassName('background')[0].style.backgroundImage = `url(${reader.result})`;
+    }
+    if (file) {
+        reader.readAsDataURL(file);
+    } else {
+        console.error('file falsy');
+    }
+}
+
+function onFileSelected(evt) {
+    const fileInput = evt.target;
+    const files = Array.from(evt.target.files).map(file => file.name);
+    const placeholder = fileInput.getAttribute('data-placeholder') || fileInput.placeholder;
+
+    // Store original placeholder
+    fileInput.setAttribute('data-placeholder', placeholder);
+
+    // Update current placeholder
+    if (files.length === 1) {
+        fileInput.placeholder = files[0].replace(/.*[\/\\]/, '');
+    }
+    else if (files.length > 1) {
+        fileInput.placeholder = `${files.length} files`;
+    }
+    else {
+        fileInput.placeholder = placeholder;
+    }
+}
+
+document.querySelectorAll('input[type="file"]').forEach(el =>
+    el.addEventListener('change', onFileSelected)
+);
